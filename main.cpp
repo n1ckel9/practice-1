63c210f12446aaa41e96e2bb0ce5673b56209e1d
n1ckel9 <a.a.ksenofontov@gmail.com>
2025-09-23 21:33:34 +0300

    added parser, counter

--- main.cpp
+++ main.cpp
@@ -1,81 +1,117 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+namespace {
 
-bool is(char* first, const char* second) {
-  int ind = 0;
-  bool flag = true;
-  while (first[ind] != '\0' && second[ind] != '\0') {
-    if (first[ind] != second[ind]) {
-      flag = false;
+const char* kWordArg = "--word";
+const char* kFileArg = "--file";
+const int kMaxWordLength = 32;
+const int kBufferSize = 4096;
+
+bool stringEquals(const char* first, const char* second) {
+    return std::strcmp(first, second) == 0;
+}
+
+bool parseArgs(int argc, char** argv, char** word, char** filename) {
+
+    bool wordProvided = false;
+    bool fileProvided = false;
+    for (int i = 1; i < argc; i++) {
+        if (stringEquals(kWordArg, argv[i])) {
+            if (i + 1 < argc) {
+                *word = argv[++i];
+                wordProvided = true;
+            }
+            else {
+                std::cerr << "Error: Missing value for --word\n";
+                return false;
+            }
+        }
+        else if (stringEquals(kFileArg, argv[i])) {
+            if (i + 1 < argc) {
+                *filename = argv[++i];
+                fileProvided = true;
+            }
+            else {
+                std::cerr << "Error: Missing value for --file\n";
+                return false;
+            }
+        }
+        else {
+            std::cerr << "Error: Unknown argument: " << argv[i] << "\n";
+            return false;
+        }
     }
-    ind++;
-  }
-  return flag && first[ind] == '\0' && second[ind] == '\0';
+    return wordProvided && fileProvided;
+}
+
+int countWords(const char* filename, const char* word) {
+    std::fstream file(filename, std::ios::in);
+    if (!file.is_open()) {
+        return -1;
+    }
+    int count = 0;
+    char buffer[kBufferSize];
+
+    bool matches[kMaxWordLength] = {false};
+
+    while (file.read(buffer, kBufferSize) || file.gcount() > 0) {
+        int bytes_read = file.gcount();
+        for (int i = 0; i < bytes_read; i++) {
+            if (buffer[i] == '\n' || buffer[i] == ' ') {
+                int ind = 0;
+                bool flag = true;
+                while (word[ind] != '\0') {
+                    flag = flag && matches[ind];
+                    ind++;
+                }
+                if (flag) {
+                    count++;
+                }
+                for (int j = 0; j < kMaxWordLength; j++) {
+                    matches[j] = false;
+                }
+                continue;
+            }
+            int ind = 0;
+            while (word[ind] != '\0') {
+                if (word[ind] == buffer[i]) {
+                    matches[ind] = true;
+                    break;
+                }
+                ind++;
+            }
+        }
+
+    }
+    return count;
+}
+
 }
 int main(int argc, char** argv) {
     char* word;
     char* file_name;
-    const char* word_arg = "--word";
-    const char* file_arg = "--file";
-    if (argc != 5) {
-      std::cout << "ERR\n";
-      return -1;
-    }
-    for (int i = 1; i < 5; i++) {
-      char* arg = argv[i];
-      if (is(arg, word_arg)) {
-        word = argv[++i];
-      }
-      else if (is(arg, file_arg)) {
-        file_name = argv[++i];
-      }
-      else {
-        std::cout << "ERR";
-        return -1;
-      }
-    }
-    //std::cout << word << " " << file_name << '\n';
-
-    std::fstream file(file_name, std::ios::in);
-    int count = 0;
-    if (file.is_open()) {
-      while (!file.eof()) {
-        char c;
-        file.get(c);
-        bool matches[32];
-        for (int i = 0; i < 32; i++) {
-          matches[i] = false;
-        }
-        while (!(c == ' ' || c == '\n')) {
-          int ind = 0;
-          while (word[ind] != '\0') {
-            if (c == word[ind]) {
-              matches[ind] = true;
-            }
-            ind++;
-          }
-          file.get(c);
-        }
-        int ind = 0;
-        bool flag = true;
-        while (word[ind] != '\0') {
-          flag = flag && matches[ind];
-          ind++;
-        }
-        if (flag) {
-          count++;
-        }
-      }
-      std::cout << count;
-
-    }
-    else {
-      std::cout << "ERR";
-      return -1;
-    }
-
-
-
     
+    if (!parseArgs(argc, argv, &word, &file_name)) {
+        std::cerr << "Error: Incorrect usage! Use " << argv[0] << 
+        " --file <file name> --word <word> ";
+        return 1;
+    }
+    
+    //std::cout << word << " " << file_name;
 
+    if (std::strlen(word) > 32) {
+        std::cerr << "Error: Invalid word length. Max length is 32 symbols";
+        return 3;
+    }
+
+    int count = countWords(file_name, word);
+
+    if (count < 0) {
+        std::cerr << "Error: File not opened!";
+        return 2;
+    }
+
+    std::cout << count;
+    return 0;
 }
